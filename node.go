75eda8f5bc6edb8fbfa6a033d2b63f4a9e8e7503
Pieter Droogendijk <pieter@binky.org.uk>
2015-04-14 01:15:40 +0200

    Add some basic tree functions

--- node.go
+++ node.go
@@ -4,12 +4,54 @@ type rawNode struct {
 	Id        NodeId
 	ParentIds []NodeId // Every ancestor id, all the way up to the root
 	ChildIds  []NodeId // Every direct child id of this node
-	Pairs     []Pair   // Key/value pairs
-	Flags     []Flag   // Value-only flags
+	Parents   []*Node  // Every ancestor, all the way up to the root
+	Children  []*Node  // Every direct child of this node
 }
 
 type Node struct {
-	rawNode
+	Pairs    []Pair  // Key/value pairs
+	Flags    []Flag  // Value-only flags
+	Parent   *Node   // Direct ancestor of this node
+	Children []*Node // Direct children of this node
 	Parents  []*Node // Every ancestor, all the way up to the root
-	Children []*Node // Every direct child of this node
+}
+
+// Create a new, empty, node.
+func NewNode() *Node {
+	return &Node{}
+}
+
+// Add a new node between the given node and its parent.
+func (node *Node) Split() *Node {
+	newNode := NewNode()
+	parent := node.Parent
+
+	if parent != nil {
+		parent.delChild(node)
+		parent.addChild(newNode)
+	}
+	newNode.addChild(node)
+	return newNode
+}
+
+// Add a new child to the given node.
+func (node *Node) AddChild() *Node {
+	newNode := NewNode()
+	node.addChild(newNode)
+	return newNode
+}
+
+func (node *Node) addChild(child *Node) {
+	child.Parent = node
+	node.Children = append(node.Children, child)
+}
+
+func (node *Node) delChild(child *Node) {
+	for i := 0; i < len(node.Children); i++ {
+		if node.Children[i] == child {
+			node.Children[i] = node.Children[len(node.Children)-1]
+			node.Children = node.Children[:len(node.Children)-1]
+			child.Parent = nil
+		}
+	}
 }
