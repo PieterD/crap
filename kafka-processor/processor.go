6fe35792cdd1988b59ae294e0feb6a581e15ddb9
Pieter Droogendijk <pieter@binky.org.uk>
2015-04-17 10:35:25 +0200

    Move procconsumer and consumerlist

--- kafka-processor/processor.go
+++ kafka-processor/processor.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
-	"sync/atomic"
 	"syscall"
 
 	"github.com/Shopify/sarama"
@@ -90,62 +88,4 @@ func main() {
 	fmt.Printf("wait\n")
 	<-cl.killchan
 	fmt.Printf("waited\n")
-}
-
-type ProcConsumer struct {
-	consumer  sarama.PartitionConsumer
-	producer  sarama.SyncProducer
-	src, dst  string
-	partition int32
-	function  []string
-	killchan  chan struct{}
-	isclosed  int32
-}
-
-func (pc *ProcConsumer) Close() {
-	if atomic.CompareAndSwapInt32(&pc.isclosed, 0, 1) {
-		pc.consumer.Close()
-		<-pc.killchan
-	}
-}
-
-func (pc *ProcConsumer) Run() {
-	defer pc.Close()
-	defer close(pc.killchan)
-	for msg := range pc.consumer.Messages() {
-		//TODO: more general function
-		newval := bytes.ToUpper(msg.Value)
-		message := &sarama.ProducerMessage{
-			Topic:     pc.dst,
-			Partition: pc.partition,
-			Value:     sarama.ByteEncoder(newval),
-		}
-		_, _, err := pc.producer.SendMessage(message)
-		if err != nil {
-			logger.Panicf("Failed to send message: %v", err)
-		}
-	}
-}
-
-type ConsumerList struct {
-	pc       []*ProcConsumer
-	killchan chan struct{}
-}
-
-func NewConsumerList() *ConsumerList {
-	cl := new(ConsumerList)
-	cl.killchan = make(chan struct{})
-	return cl
-}
-
-func (cl *ConsumerList) Close() {
-	defer close(cl.killchan)
-	for i := range cl.pc {
-		cl.pc[i].Close()
-	}
-}
-
-func (cl *ConsumerList) Add(pc *ProcConsumer) {
-	go pc.Run()
-	cl.pc = append(cl.pc, pc)
 }
