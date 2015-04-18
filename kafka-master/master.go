7c9d29f5431e13eb70377c4aa44c3158d006d8f9
Pieter Droogendijk <pieter@binky.org.uk>
2015-04-18 21:34:00 +0200

    Move stuff out of kafka-master

--- kafka-master/master.go
+++ kafka-master/master.go
@@ -5,11 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
-	"time"
-
-	"github.com/Shopify/sarama"
-	"github.com/samuel/go-zookeeper/zk"
 )
 
 var (
@@ -30,27 +25,4 @@ func main() {
 		flagbad("No zookeeper peers provided; use -peers or the ZOOKEEPER_PEERS environment variable")
 	}
 
-	zooConn, _, err := zk.Connect(strings.Split(*fPeers, ","), time.Second)
-	if err != nil {
-		logger.Panicf("Failed to connect to zookeeper: %v", err)
-	}
-	defer zooConn.Close()
-
-	kafkaBrokers := GetKafkaBrokers(zooConn)
-	if len(kafkaBrokers) == 0 {
-		logger.Panicf("No kafka brokers found!")
-	}
-	for _, broker := range kafkaBrokers {
-		fmt.Printf("broker %3d, addr: %s\n", broker.Id, broker.Addr())
-	}
-
-	config := sarama.NewConfig()
-	config.Producer.Partitioner = sarama.NewRandomPartitioner
-	config.Producer.RequiredAcks = sarama.WaitForAll
-	config.ClientID = "kafkaproc.master"
-	client, err := sarama.NewClient(KafkaBrokerStrings(kafkaBrokers), config)
-	if err != nil {
-		logger.Panicf("Creating sarama client: %v", err)
-	}
-	defer client.Close()
 }
