673f0930f2f30ce20a584d108104651bbbaf768e
Pieter Droogendijk <pieter@binky.org.uk>
2015-04-22 02:06:04 +0200

    severely simplified processor

--- tools/kafka-processor/processor.go
+++ tools/kafka-processor/processor.go
@@ -1,19 +1,25 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/PieterD/kafka"
+	"github.com/PieterD/kafka/internal/killchan"
 )
 
 var (
-	fConfig  = flag.String("config", "", "xml file configuring processing streams")
-	fVerbose = flag.Bool("verbose", false, "Be more verbose")
+	fPeers       = flag.String("peers", os.Getenv("ZOOKEEPER_PEERS"), "List of Zookeeper peer addresses (Defaults to ZOOKEEPER_PEERS env)")
+	fPartition   = flag.Int("partition", -1, "Partition to receive from")
+	fSource      = flag.String("src", "", "Topic to receive from")
+	fDestination = flag.String("dst", "", "Topic to send to")
+	fVerbose     = flag.Bool("verbose", false, "Be more verbose")
 
 	logger = log.New(os.Stderr, "processor", log.LstdFlags)
 )
@@ -26,33 +32,66 @@ func flagbad(f string, i ...interface{}) {
 
 func main() {
 	flag.Parse()
-	if *fConfig == "" {
-		flagbad("-config is empty\n")
+	if *fPeers == "" {
+		flagbad("-peers is empty\n")
+	}
+	if *fPartition == -1 {
+		flagbad("-partition is empty\n")
+	}
+	if *fSource == "" {
+		flagbad("-src is empty\n")
+	}
+	if *fDestination == "" {
+		flagbad("-dst is empty\n")
 	}
 
-	cfg, err := ParseConfig(*fConfig)
+	kfk, err := kafka.New("kafka-processor", logger, strings.Split(*fPeers, ","))
 	if err != nil {
-		logger.Panicf("Failed to parse config: %v", err)
-	}
-
-	kfk, err := kafka.New("kafka-processor", logger, cfg.Zookeeper.Peer)
-	if err != nil {
-		logger.Panicf("Failed to start kafka")
+		logger.Panicf("Failed to start kafka: %v\n", err)
 	}
 	defer kfk.Close()
 
-	th, err := newThreadHandler(kfk, cfg)
+	err = kfk.ListenNewest(*fSource, int32(*fPartition))
 	if err != nil {
-		logger.Panicf("Failed to start thread handler")
+		logger.Panicf("Failed to listen to partition %s:%d: %v\n", *fSource, *fPartition)
 	}
+	defer kfk.Unlisten(*fSource, int32(*fPartition))
+
+	kill := killchan.New()
 
 	sigchan := make(chan os.Signal)
 	signal.Notify(sigchan, syscall.SIGINT)
 	go func() {
 		<-sigchan
-		th.close()
+		kill.Kill()
 		<-sigchan
 		panic("second SIGINT")
 	}()
-	th.wait()
+
+	for {
+		var msg kafka.Message
+
+		select {
+		case <-kill.Chan():
+			return
+		case msg = <-kfk.Incoming():
+		}
+
+		// PROCESSING
+		newVal := bytes.ToUpper(msg.Val)
+		// PROCESSING
+
+		select {
+		case <-kill.Chan():
+			return
+		case trans := <-kfk.Outgoing():
+			partition, offset, err := trans.Send(msg.Key, newVal, *fDestination)
+			if err != nil {
+				logger.Panicf("Message send failed to %s: %v", *fDestination, err)
+			}
+			if *fVerbose {
+				fmt.Printf("processed %s:%d(%d) -> %s:%d(%d)\n", msg.Topic, msg.Partition, msg.Offset, *fDestination, partition, offset)
+			}
+		}
+	}
 }
