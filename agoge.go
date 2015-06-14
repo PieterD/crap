79034d8fd3f3316dfd4558b04ab3e9e4894329ee
Pieter Droogendijk <pieter@binky.org.uk>
2015-06-14 23:25:25 +0200

    Move htmlcmd to internal

--- agoge.go
+++ agoge.go
@@ -2,7 +2,7 @@ package main
 
 import "github.com/PieterD/agoge/internal/flags"
 
-import _ "github.com/PieterD/agoge/html/htmlcmd"
+import _ "github.com/PieterD/agoge/internal/htmlcmd"
 
 func main() {
 	flags.Run()
