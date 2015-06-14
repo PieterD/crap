4a705f4161799164446523d08b452bb302597fde
Pieter Droogendijk <pieter@binky.org.uk>
2015-06-14 23:27:14 +0200

    Hm

--- agoge.go
+++ agoge.go
@@ -2,7 +2,7 @@ package main
 
 import "github.com/PieterD/agoge/internal/flags"
 
-import _ "github.com/PieterD/agoge/internal/htmlcmd"
+import _ "github.com/PieterD/agoge/internal/commands/html"
 
 func main() {
 	flags.Run()
