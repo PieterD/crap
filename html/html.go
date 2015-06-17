c6b6c18c4f03e40a9113c3025e22129632ede35c
Pieter Droogendijk <pieter@binky.org.uk>
2015-06-17 23:14:32 +0200

    Hm.

--- html/html.go
+++ html/html.go
@@ -2,6 +2,7 @@ package html
 
 import (
 	"go/ast"
+	"go/parser"
 	"go/printer"
 	"go/token"
 	"os"
@@ -25,11 +26,18 @@ func Incorporate(pkg string, outfile string, infile ...string) error {
 					List: []*ast.Field{},
 				},
 			},
-			//Body: &ast.BlockStmt{
-			//	List: []ast.Stmt{},
-			//},
+			Body: &ast.BlockStmt{
+				List: []ast.Stmt{
+					buildInit(infile),
+				},
+			},
 		},
 	)
 	printer.Fprint(os.Stdout, fset, file)
+	return nil
+}
+
+func buildInit(infiles []string) []ast.Stmt {
+	parser.ParseExpr("tmpl.New(\"style.css\")")
 	return nil
 }
