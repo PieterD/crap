5f764567836217c570f177b826687cb7c1bbb063
Pieter Droogendijk <pieter@binky.org.uk>
2015-06-25 23:03:17 +0200

    Bolt?

--- moogle/moogle.go
+++ moogle/moogle.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/boltdb/bolt"
+)
+
+func main() {
+	err := run()
+	if err != nil {
+		log.Printf("Moogle failed: %v", err)
+	}
+}
+
+func run() error {
+	db, err := bolt.Open(*fDatabase, 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		return err
+	}
+	return nil
+}
