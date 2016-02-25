6a56c97675f94074f0dfd951b49ea36e1025488b
Pieter Droogendijk <pieter@binky.org.uk>
2016-02-25 11:09:55 +0100

    Added createshader and errors, getting strange signal

--- examples/jason01/main.go
+++ examples/jason01/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "github.com/PieterD/crap/glimmer"
+import (
+	"fmt"
+
+	"github.com/PieterD/crap/glimmer"
+	"github.com/go-gl/glfw/v3.1/glfw"
+)
+
+type Profile struct {
+	glimmer.DefaultProfile
+}
+
+func (p Profile) PostCreation(w *glfw.Window) {
+	p.DefaultProfile.PostCreation(w)
+	shader, err := glimmer.CreateShader(glimmer.VertexShader, []byte(`
+	#version 330
+	layout(location = 0) in vec4 position;
+	ajepgoaejg
+	void main() {
+		gl_Position = position;
+	}
+	`))
+	if err != nil {
+		fmt.Printf("Error compiling shader: %v\n", err)
+	}
+	shader.Delete()
+}
 
 func main() {
-	glimmer.Run(glimmer.DefaultProfile{})
+	glimmer.Run(Profile{})
 }
