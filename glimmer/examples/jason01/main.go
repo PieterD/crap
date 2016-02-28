a0690cfb603864db8a16bb923ecb5830ec236a83
Pieter Droogendijk <pieter@binky.org.uk>
2016-02-28 23:32:04 +0100

    Clean up, some kind of error handling

--- glimmer/examples/jason01/main.go
+++ glimmer/examples/jason01/main.go
@@ -18,50 +18,52 @@ type Profile struct {
 	arrayPointer   *glimmer.ArrayPointer
 }
 
-func (p *Profile) PostCreation(w *glfw.Window) {
-	p.DefaultProfile.PostCreation(w)
-	var err error
-	p.vertexShader, err = glimmer.CreateShader(glimmer.VertexShader, `
-	#version 330
-	layout(location = 0) in vec4 position;
-	void main() {
-		gl_Position = position;
-	}
-	`)
-	if err != nil {
-		fmt.Printf("Error compiling vertex shader: %v\n", err)
-		return
-	}
+var vertexShaderText = `
+#version 330
+layout(location = 0) in vec4 position;
+void main() {
+	gl_Position = position;
+}
+`
 
-	p.fragmentShader, err = glimmer.CreateShader(glimmer.FragmentShader, `
+var fragmentShaderText = `
 	#version 330
 	out vec4 outputColor;
 	void main() {
 		outputColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
 	}
-	`)
+`
+
+var vertexData = []float32{
+	0.75, 0.75, 0.0, 1.0,
+	0.75, -0.75, 0.0, 1.0,
+	-0.75, -0.75, 0.0, 1.0,
+}
+
+func (p *Profile) PostCreation(w *glfw.Window) (err error) {
+	p.DefaultProfile.PostCreation(w)
+	p.vertexShader, err = glimmer.CreateShader(glimmer.VertexShader, vertexShaderText)
 	if err != nil {
-		fmt.Printf("Error compiling fragment shader: %v\n", err)
-		return
+		return fmt.Errorf("Error compiling vertex shader: %v", err)
+	}
+
+	p.fragmentShader, err = glimmer.CreateShader(glimmer.FragmentShader, fragmentShaderText)
+	if err != nil {
+		return fmt.Errorf("Error compiling fragment shader: %v\n", err)
 	}
 
 	p.program, err = glimmer.CreateProgram(p.vertexShader, p.fragmentShader)
 	if err != nil {
-		fmt.Printf("Error linking program: %v\n", err)
-		return
+		return fmt.Errorf("Error linking program: %v\n", err)
 	}
 
 	p.vertexArray = glimmer.CreateVertexArray()
 	p.buffer = glimmer.CreateBuffer()
 	p.buffer.UseAsArrayBuffer()
-	p.buffer.FloatData([]float32{
-		0.75, 0.75, 0.0, 1.0,
-		0.75, -0.75, 0.0, 1.0,
-		-0.75, -0.75, 0.0, 1.0,
-	})
+	p.buffer.FloatData(vertexData)
 	p.arrayPointer = p.buffer.Pointer(4, false, 0, 0)
 
-	fmt.Printf("initialization: %v\n", glimmer.GetError())
+	return glimmer.GetError()
 }
 
 func (p *Profile) End() {
@@ -72,14 +74,18 @@ func (p *Profile) End() {
 	p.buffer.Delete()
 }
 
-func (p *Profile) Draw(w *glfw.Window) {
+func (p *Profile) Draw(w *glfw.Window) error {
 	gl.ClearColor(0.0, 0.0, 0.0, 0.0)
 	gl.Clear(gl.COLOR_BUFFER_BIT)
 	p.program.Use()
 	p.vertexArray.Enable(0, p.arrayPointer)
 	gl.DrawArrays(gl.TRIANGLES, 0, 3)
+	return nil
 }
 
 func main() {
-	glimmer.Run(&Profile{})
+	err := glimmer.Run(&Profile{})
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
 }
