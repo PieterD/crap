5dfca87c57b7f262e55a326ab35d37ea0c15db13
Pieter Droogendijk <pieter@binky.org.uk>
2016-03-04 17:12:38 +0100

    Moved vertexarray to gli

--- glimmer/gli/context.go
+++ glimmer/gli/context.go
@@ -12,8 +12,11 @@ type iContext struct {
 type Context interface {
 	CreateShader(shaderType ShaderType) Shader
 	CreateProgram() Program
+	CreateVertexArrayObject() VertexArrayObject
 	UseProgram(Program)
 	UseNoProgram()
+	BindVertexArrayObject(vao VertexArrayObject)
+	UnbindVertexArrayObject()
 }
 
 func CreateContext() Context {
