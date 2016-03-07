e9e3ff7c906cff4590903b1d2cc6ca782f045c98
Pieter Droogendijk <pieter@binky.org.uk>
2016-03-07 23:34:11 +0100

    Start work on buffer

---
+++ glimmer/gli/buffer.go
@@ -0,0 +1,71 @@
+package gli
+
+import "github.com/go-gl/gl/v3.3-core/gl"
+
+type Buffer interface {
+	Id() uint32
+	Delete()
+}
+
+type iBuffer struct {
+	id         uint32
+	targethint BufferTarget
+}
+
+type BufferAccessTypeHint uint32
+
+const (
+	StaticDraw  BufferAccessTypeHint = gl.STATIC_DRAW
+	StaticRead  BufferAccessTypeHint = gl.STATIC_READ
+	StaticCopy  BufferAccessTypeHint = gl.STATIC_COPY
+	StreamDraw  BufferAccessTypeHint = gl.STREAM_DRAW
+	StreamRead  BufferAccessTypeHint = gl.STREAM_READ
+	StreamCopy  BufferAccessTypeHint = gl.STREAM_COPY
+	DynamicDraw BufferAccessTypeHint = gl.DYNAMIC_DRAW
+	DynamicRead BufferAccessTypeHint = gl.DYNAMIC_READ
+	DynamicCopy BufferAccessTypeHint = gl.DYNAMIC_COPY
+)
+
+type BufferTarget uint32
+
+const (
+	ArrayBuffer             BufferTarget = gl.ARRAY_BUFFER
+	AtomicCounterBuffer     BufferTarget = gl.ATOMIC_COUNTER_BUFFER
+	CopyReadBuffer          BufferTarget = gl.COPY_READ_BUFFER
+	CopyWriteBuffer         BufferTarget = gl.COPY_WRITE_BUFFER
+	DrawIndirectBuffer      BufferTarget = gl.DRAW_INDIRECT_BUFFER
+	DispatchIndirectBuffer  BufferTarget = gl.DISPATCH_INDIRECT_BUFFER
+	ElementArrayBuffer      BufferTarget = gl.ELEMENT_ARRAY_BUFFER
+	PixelPackBuffer         BufferTarget = gl.PIXEL_PACK_BUFFER
+	PixelUnpackBuffer       BufferTarget = gl.PIXEL_UNPACK_BUFFER
+	QueryBuffer             BufferTarget = gl.QUERY_BUFFER
+	ShaderStorageBuffer     BufferTarget = gl.SHADER_STORAGE_BUFFER
+	TextureBuffer           BufferTarget = gl.TEXTURE_BUFFER
+	TransformFeedbackBuffer BufferTarget = gl.TRANSFORM_FEEDBACK_BUFFER
+	UniformBuffer           BufferTarget = gl.UNIFORM_BUFFER
+)
+
+func (context iContext) BindBuffer(target BufferTarget, buffer Buffer) {
+	gl.BindBuffer(uint32(target), buffer.Id())
+}
+
+func (context iContext) UnbindBuffer(target BufferTarget) {
+	gl.BindBuffer(uint32(target), 0)
+}
+
+func (context iContext) CreateBuffer(accesshint BufferAccessTypeHint, targethint BufferTarget) Buffer {
+	var id uint32
+	gl.GenBuffers(1, &id)
+	return iBuffer{id: id, targethint: targethint}
+}
+
+func (buffer iBuffer) Id() uint32 {
+	return buffer.id
+}
+
+func (buffer iBuffer) Delete() {
+	gl.DeleteBuffers(1, &buffer.id)
+}
+
+func (buffer iBuffer) DataSlice(iface interface{}) {
+}
